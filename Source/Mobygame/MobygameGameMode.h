60da8710150af440a88f0995582917fce7bfde48
FishKing <2788579145@qq.com>
2022-12-02 20:10:54 +0800

    init

---
+++ Source/Mobygame/MobygameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MobygameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMobygameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMobygameGameMode();
+};
+
+
+
